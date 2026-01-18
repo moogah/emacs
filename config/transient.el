edea956a1104a93315f8eb4cdf65b8a8ba89f923
Jeff Farr <moogah@gmail.com>
2026-01-18 12:53:26 +0100

    Force immediate loading of transient package with :demand t
    - Add :demand t to transient use-package declaration
    - Ensures newer transient version loads immediately
    - Overrides built-in Emacs 30.2 v0.6.0 before magit loads
    - Fixes void-function transient--set-layout error

--- config/transient.el
+++ config/transient.el
@@ -1,5 +1,6 @@
 (use-package transient
-  :straight t)  ; Use latest version instead of old v0.6.0
+  :straight t  ; Use latest version instead of old v0.6.0
+  :demand t)   ; Force immediate load to override built-in version
 
 (use-package transient-showcase
   :straight '(transient-showcase :type git :host github :repo "positron-solutions/transient-showcase"))
