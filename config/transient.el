e82d829d8006afa6efd675991cc85f51e9adc66d
Jeff Farr <moogah@gmail.com>
2026-01-18 12:57:39 +0100

    Add debug output to diagnose transient loading
    - Check if transient already loaded before use-package
    - Print where transient is loaded from after use-package
    - Verify transient--set-layout function exists
    - Help diagnose built-in vs straight.el version conflict

--- config/transient.el
+++ config/transient.el
@@ -1,6 +1,21 @@
+;; Debug: Check state before loading transient
+(message "=== TRANSIENT DEBUG ===")
+(message "Before: transient loaded? %s" (featurep 'transient))
+(when (featurep 'transient)
+  (message "Before: transient-version = %s"
+           (if (boundp 'transient--version) transient--version "undefined")))
+
 (use-package transient
   :straight t  ; Use latest version instead of old v0.6.0
-  :demand t)   ; Force immediate load to override built-in version
+  :demand t    ; Force immediate load to override built-in version
+  :config
+  (message "After: transient loaded from %s"
+           (locate-library "transient"))
+  (message "After: transient-version = %s"
+           (if (boundp 'transient--version) transient--version "undefined"))
+  (message "After: has transient--set-layout? %s"
+           (fboundp 'transient--set-layout))
+  (message "=== END TRANSIENT DEBUG ==="))
 
 (use-package transient-showcase
   :straight '(transient-showcase :type git :host github :repo "positron-solutions/transient-showcase"))
